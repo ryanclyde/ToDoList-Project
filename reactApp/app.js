5415b2c5d54120c19bd87c56f922f356323a5a1b
Ryan Clyde <rclyde@wharton.upenn.edu>
2017-06-30 18:22:17 -0700

    button X

--- reactApp/app.js
+++ reactApp/app.js
@@ -1,8 +1,35 @@
 // alert('app.js ran!');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+var dummyData = ["Get notes to study", "Conept Tracker", "apply to JPMorg", "Get sleep", "Plan weekend"];
 
-ReactDOM.render(<p>React lives! MWAHAHAHA </p>,
+
+class ToDo extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return(
+      <li><button>X</button>{this.props.task}</li>
+    )
+  }
+}
+
+
+class ToDoList extends React.Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    return(
+      <ul>
+        {dummyData.map((task) => <ToDo task = {task}/>)}
+      </ul>
+    )
+  }
+}
+
+ReactDOM.render(<ToDoList />,
    document.getElementById('root'));
